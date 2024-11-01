1701486d5366704006f60b0a1838316f3c04c1a1
LillyMS <lilly.marlene.siewert@ut.ee>
2024-11-01 22:02:41 +0200

    fixing bug & scroll to bottom function

--- frontend/res/js/postMessages.js
+++ frontend/res/js/postMessages.js
@@ -4,22 +4,49 @@ window.onload = async function() {
     // all of the other should be redirected to the login page
 
     // Setting active element
+    
     activeElement = document.getElementsByClassName("on");
     activeSelection = sessionStorage.getItem('activeElement');
-    if (activeElement.length != 1){
-        document.getElementById("planning").classList.toggle("on");
-    }else if (activeElement[0].id != activeSelection){
-        activeElement[0].classList.toggle("on");
-        document.getElementById(activeSelection).classList.toggle("on");
-    }
+    setActiveElement(activeElement, activeSelection);
+    
 
-    if (sessionStorage.getItem("activeElement") == "tips"){
-        postTips();
-    } else {
+    if (sessionStorage.getItem("activeElement") != "tips"){
 
     //TODO fetch messages from DB - we need to know which was an answer & which a prompt to which topic
     
     //TODO fetch message response from AI, display and save in DB
 
     }
+
+    function setActiveElement(activeElement, activeSelection){
+        if (activeSelection == null){
+            return document.getElementById("planning").classList.toggle("on");
+        }
+        
+        if (activeElement.length == 0){
+            return document.getElementById(activeSelection).classList.toggle("on");
+        } 
+        
+        if (activeElement[0].id != activeSelection){
+            activeElement[0].classList.toggle("on");
+            return document.getElementById(activeSelection).classList.toggle("on");
+        }
+    }
+
+    scrollToBottom();
+}
+
+function scrollToBottom(timedelay=0) {
+    var scrollId;
+    var height = 0;
+    var minScrollHeight = 100;
+    scrollId = setInterval(function () {
+        if (height <= document.body.scrollHeight) {
+            window.scrollBy(0, minScrollHeight);
+        }
+        else {
+            clearInterval(scrollId);
+        }
+        height += minScrollHeight;
+    }, timedelay);           
 }
