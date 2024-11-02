f7482af9b598f2c29b2795ac5af2518856393dfb
LillyMS <lilly.marlene.siewert@ut.ee>
2024-11-02 20:03:21 +0200

    save toggle darkmode

--- frontend/res/js/generalEvents.js
+++ frontend/res/js/generalEvents.js
@@ -2,4 +2,10 @@ function openHelpPage () {
     window.location.href = "../../../frontend/pages/help.html";
 }
 
+function openMenuPopup () {
+    document.getElementById("menu_container").classList.toggle("open");
+
+}
+
 document.getElementById("help").addEventListener("click", openHelpPage);
+document.getElementById("menu").addEventListener("click", openMenuPopup);
