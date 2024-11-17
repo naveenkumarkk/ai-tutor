84733530ef93f8d6c3a1f03baf890ffcbfbb5af6
LillyMS <lilly.marlene.siewert@ut.ee>
2024-11-17 14:46:18 +0200

    fix responsive design

--- frontend/res/js/generalEvents.js
+++ frontend/res/js/generalEvents.js
@@ -35,15 +35,10 @@ function handleLogout(event) {
 document.getElementById("help")?.addEventListener("click", openHelpPage);
 document.getElementById("menu")?.addEventListener("click", openMenuPopup);
 
-document.getElementById("sidebar-toggle")?.addEventListener("click", () => {
-  if (window.innerWidth <= 600) { // Ensure it only runs for mobile sizes
-    const sidebar = document.querySelector(".sidebar");
-    sidebar.classList.toggle("hidden"); 
+document.getElementById("toggleSidebar")?.addEventListener("click", () => {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("folded");
 
-    if (sidebar.style.visibility === "visible") {
-      sidebar.style.visibility = "hidden";
-    } else {
-      sidebar.style.visibility = "visible";
-    }
-  }
+  const chat = document.querySelector('.chat');
+  chat.classList.toggle("expanded");
 });
